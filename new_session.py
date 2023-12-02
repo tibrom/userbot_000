1ee25ef7783a3d684fa8e7afcf6b2b70053f64a8
Andrey <gorlans@mail.ru>
2023-12-02 12:19:03 +0300

    sss

--- new_session.py
+++ new_session.py
@@ -6,7 +6,7 @@ api_hash = "0123456789abcdef0123456789abcdef"
 
 
 async def main():
-    async with Client("my_account", api_id, api_hash) as app:
+    async with Client("session23", api_id, api_hash) as app:
         await app.send_message("me", "Greetings from **Pyrogram**!")
 
 
