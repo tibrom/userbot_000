854d10f0fb0b6f5677e14014dfc6ea17d0ae31fc
Andrey <gorlans@mail.ru>
2023-12-14 01:53:01 +0300

    ddd

--- front/app.py
+++ front/app.py
@@ -7,7 +7,33 @@ from data_base import get_tiggers, get_all_chat, add_tiggers, get_tigger, update
 from flask_wtf import FlaskForm, CSRFProtect
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
-from logger import logger
+
+import logging
+
+
+# Создаем и настраиваем логгер
+logger = logging.getLogger("dev_logger")
+logger.setLevel(logging.DEBUG)
+
+# Создаем обработчик (handler) для записи логов в файл
+file_handler = logging.FileHandler("app.log")
+file_handler.setLevel(logging.DEBUG)
+
+# Создаем обработчик (handler) для вывода логов на консоль
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+
+# Создаем форматер для определения формата записей в логе
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+
+# Добавляем обработчики в логгер
+#logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+
+
+
 
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
